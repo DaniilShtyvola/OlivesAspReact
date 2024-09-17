755e7ea128707101785928e7fa7fa23901b6c35e
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2024-09-17 19:07:32 +0300

    Add files via upload

---
+++ OilNewsAPI/Database/Interfaces/INewsRepository.cs
@@ -0,0 +1,14 @@
+﻿using OilNewsAPI.Database.Models;
+
+namespace OilNewsAPI.Database.Interfaces
+{
+    public interface INewsRepository
+    {
+        Task<IEnumerable<News>> GetAllAsync();
+        Task<News> GetByIdAsync(int id);
+        Task<int> CreateAsync(News news);
+        Task<int> UpdateAsync(News news);
+        Task<int> DeleteAsync(int id);
+    }
+
+}
---
+++ OilNewsAPI/Database/Models/News.cs
@@ -0,0 +1,12 @@
+﻿namespace OilNewsAPI.Database.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+
+}
