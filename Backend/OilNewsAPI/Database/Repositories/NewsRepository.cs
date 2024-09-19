e256a506f5d990af40b59fd92b3db2334f902641
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2024-09-19 17:07:56 +0300

    Add files via upload

--- Backend/OilNewsAPI/Database/Repositories/NewsRepository.cs
+++ Backend/OilNewsAPI/Database/Repositories/NewsRepository.cs
@@ -34,16 +34,20 @@ namespace OilNewsAPI.Database.Repositories
         public async Task<int> CreateAsync(News news)
         {
             using var connection = new SqlConnection(GetConnectionString());
-            var sql = "INSERT INTO News (Title, Content, Publisher, PublishDate) VALUES (@Title, @Content, @Publisher, @PublishDate)";
-            return await connection.ExecuteAsync(sql, news);
+            var sql = "INSERT INTO News (Title, Content, Icon, Publisher, PublisherIcon, PublishDate) " +
+                      "OUTPUT INSERTED.Id " +
+                      "VALUES (@Title, @Content, @Icon, @Publisher, @PublisherIcon, @PublishDate)";
+            return await connection.QuerySingleAsync<int>(sql, news);
         }
 
         public async Task<int> UpdateAsync(News news)
         {
             using var connection = new SqlConnection(GetConnectionString());
-            var sql = "UPDATE News SET Title = @Title, Content = @Content, Publisher = @Publisher, PublishDate = @PublishDate WHERE Id = @Id";
+            var sql = "UPDATE News SET Title = @Title, Content = @Content, Icon = @Icon, Publisher = @Publisher, " +
+                      "PublisherIcon = @PublisherIcon, PublishDate = @PublishDate WHERE Id = @Id";
             return await connection.ExecuteAsync(sql, news);
         }
+
 
         public async Task<int> DeleteAsync(int id)
         {
